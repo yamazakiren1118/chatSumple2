9b67cb3c1647f76e2341a560c7e16660db1c24e2
yamazakiren1118 <yamazakiren1118@yahoo.co.jp>
2022-06-30 00:22:21 +0900

    https boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,9 +26,9 @@ class AppServiceProvider extends ServiceProvider
         //
         public function boot()
         {
-            if (\App::environment('production')) {
-                \URL::forceScheme('https');
-            }
+            // if (\App::environment('production')) {
+            //     \URL::forceScheme('https');
+            // }
         }
     }
 }
